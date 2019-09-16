3497e69a626b130210cb5380f9777391200ef6d1
Christian Grimberg <christian.grimberg@outlook.com>
2019-09-15 21:04:55 -0300

    :+1: Se agrega una funcion para concatenar dos cadenas ingresadas en una nueva controlando el espacio en memoria.

--- main.c
+++ main.c
@@ -2,9 +2,19 @@
 
 int main()
 {
+    char string[STRING_MAX];
+    char firstString[] = "Hola ";
+    char secondString[51];
+    
     input_clearScreen();
 
-    input_pauseScreen("Hola mundo");
+    if(!input_getString(secondString, "Ingrese su nombre: ", "Intente nuevamente: ", 2, 51))
+    {
+        if(!input_concatStrings(string, firstString, secondString, 100))
+        {
+            input_pauseScreen(string);
+        }
+    }
 
     return 0;
 }
