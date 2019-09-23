fd15ca4d84c575392c0f89b236164483fbffa730
Christian Grimberg <christian.grimberg@outlook.com>
2019-09-23 12:20:31 -0300

    :+1: Se implementa funcion para comparar fechas y para hacer intercambio de fechas.

--- structs.c
+++ structs.c
@@ -1,1 +1,73 @@
 #include "structs.h"
+
+static int dateCompare(sDate date1, sDate date2);
+
+int structs_swapDate(sDate* date1, sDate* date2)
+{
+    int returnValue = -1;
+    sDate aux1;
+    sDate aux2;
+
+    aux1 = *date1;
+    aux2 = *date2;
+    *date1 = *date2;
+    *date2 = aux1;
+
+    if(!dateCompare(*date1, aux2) && !dateCompare(*date2, aux1))
+    {
+        returnValue = 0;
+    }
+
+    return returnValue;
+}
+
+static int dateCompare(sDate date1, sDate date2)
+{
+    int returnValue;
+
+    if(date1.year > date2.year)
+    {
+        returnValue = 1;
+    }
+    else
+    {
+        if(date1.year < date2.year)
+        {
+            returnValue = -1;
+        }
+        else
+        {
+            if(date1.month > date2.month)
+            {
+                returnValue = 1;
+            }
+            else
+            {
+                if(date1.month < date2.month)
+                {
+                    returnValue = -1;
+                }
+                else
+                {
+                    if(date1.day > date2.day)
+                    {
+                        returnValue = 1;
+                    }
+                    else
+                    {
+                        if(date1.day < date2.day)
+                        {
+                            returnValue = -1;
+                        }
+                        else
+                        {
+                            returnValue = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return returnValue;
+}
--- structs.h
+++ structs.h
@@ -3,4 +3,13 @@
 
 #include "arrays.h"
 
+/** \brief Funcion de intercambio de fechas.
+ * 
+ * \param date1 sDate Primer fecha.
+ * \param date2 sDate Segunda fecha.
+ * \return Si el intercambio se realizo devuelve [0] si no [-1].
+ * 
+ */
+int structs_swapDate(sDate* date1, sDate* date2);
+
 #endif // STRUCTS_H_INCLUDED
